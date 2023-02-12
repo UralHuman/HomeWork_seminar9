a3060cd0bf1ff613d68b55cdc12979d84478d877
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-12 21:33:15 +0500

    create code_1.0

---
+++ HomeWork_1_seminar_9/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Задайте значение (N) >>> ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int m = 1;
+
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
