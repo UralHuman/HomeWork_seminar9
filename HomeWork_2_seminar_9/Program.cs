688326faad0f9c6e293dcfa25cfb2431a8e3a47c
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-13 16:10:29 +0500

    create code_2.0

---
+++ HomeWork_2_seminar_9/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.Write("Задайте значение (n) >>> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Задайте значение (m) >>> ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($" {sum} ");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+
+}
+
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+Console.Write($"{m}, {n} ->");
+
+PrintSum(m, n, temp = 0);
