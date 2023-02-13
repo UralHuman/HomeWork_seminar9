7e28622eeb81e56dbfb3a393e8714c030b28e8b7
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-13 16:42:56 +0500

    create code_3.0

---
+++ HomeWork_3_seminar_9/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+PrintResult(FunAkk(n, m));
+
+int FunAkk(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return FunAkk(n - 1, 1);
+    if (n > 0 && m > 0) return FunAkk(n - 1, FunAkk(n, m - 1));
+    return FunAkk(n, m);
+}
+
+void PrintResult(int a)
+{
+    Console.WriteLine($"\nРезультат вычисления >>> {a}");
+}
+
+System.Console.WriteLine();
